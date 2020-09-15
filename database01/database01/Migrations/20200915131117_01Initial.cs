cc9f5e02f5437aa02907a3346421d785e0c151b0
Filip Margitai <u18.415@pslib.cloud>
2020-09-15 15:16:24 +0200

    vytvořená databáze

---
+++ database01/database01/Migrations/20200915131117_01Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace database01.Migrations
+{
+    public partial class _01Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    AnimId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Species = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Weight = table.Column<int>(nullable: true),
+                    IsAlive = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.AnimId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
