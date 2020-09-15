561fd71a5f28e6d2401ff4e0715c9e73b3a00f88
Filip Margitai <u18.415@pslib.cloud>
2020-09-15 15:03:05 +0200

    Animal class

---
+++ database01/database01/Model/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace database01.Model
+{
+    public class Animal
+    {
+        [Key]
+        public int AnimId { get; set; }
+        [Required]
+        public string Species { get; set; } // špatné, ukázka Required
+
+        public string Name { get; set; } // string => nepovinný
+        public int? Weight { get; set; } // ? => nepovinný, normální int je povinný
+        public bool IsAlive { get; set; }
+    }
+}
